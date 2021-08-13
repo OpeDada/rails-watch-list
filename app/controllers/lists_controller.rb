62a74635f0c134b9cc9fb86aab49a8d043e48b3a
OpeDada <opedada@gmail.com>
2021-08-13 11:02:33 +0900

    watchlist app in progress

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,33 @@
+class ListsController < ApplicationController
+
+  before_action :set_params, only: [:show]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to list_path(@list) #show path
+    else
+      render :new
+    end
+  end
+
+  def set_params
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
