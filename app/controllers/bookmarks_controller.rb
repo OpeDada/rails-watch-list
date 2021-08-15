7f0999e1cd8a1dd4a082f26baf3241df6b9ecec9
OpeDada <opedada@gmail.com>
2021-08-16 00:36:56 +0900

    nothing changed

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,5 @@
 class BookmarksController < ApplicationController
-  before_action :set_params, only: [:new, :create, ]
+  before_action :set_params, only: [:new, :create]
 
   def new
     @bookmark = Bookmark.new
